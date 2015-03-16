d9c3b62764bb62f36453c2d2641a251fd981b204
Anisimov Ruslan <anisimov1337@mail.ru>
2015-03-16 18:43:53 +0400

    C

---
+++ C#/Minicalculator/Minicalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double D1 = 0, D2 = 0, AN = 0;
+            string ZN = "";
+            Console.Write("Число 1: ");
+            D1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Число 2: ");
+            D2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите знак действия(+, -, *, /, SQR: ");
+            ZN = Console.ReadLine();
+            if (ZN == "+")
+            {
+                AN = D1 + D2;
+                Console.WriteLine("Ответ: " + Convert.ToString(AN));
+            }
+            if (ZN == "-") 
+            {
+                AN = D1 - D2;
+                Console.WriteLine("Ответ: " + Convert.ToString(AN));
+            }
+            if (ZN == "*")
+            {
+                AN = D1 * D2;
+                Console.WriteLine("Ответ: " + Convert.ToString(AN));
+            }
+            if (ZN == "/")
+            {
+                AN = D1 / D2;
+                Console.WriteLine("Ответ: " + Convert.ToString(AN));
+            }
+            else
+            {
+                Console.WriteLine("Такого знака не существует.");
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ C#/Screen auto/WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var bounds = Screen.GetBounds(new Point(0, 0));
+            var jpeg = new Bitmap(bounds.Width, bounds.Height);
+            using (var g = Graphics.FromImage(jpeg))
+                g.CopyFromScreen(0, 0, 0, 0, jpeg.Size);
+            jpeg.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "screenshot.jpeg"));
+            this.Close();
+            Form2 Form2 = new Form2();
+            Form2.Show();
+        }
+    }
+}
